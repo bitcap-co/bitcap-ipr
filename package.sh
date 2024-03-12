03f34a6be1a7d2aad0c53fc42c169c0b059be086
MatthewWertman <matthewwertman57@gmail.com>
2024-03-11 23:46:16 -0700

    update linux package

--- package.sh
+++ package.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 ## build ipr linux package with fpm
 
-PNAME="bitcap-ipreporter"
+PNAME="bitcapipr-linux-x86_64"
 
 # create dirs
-[ -e package] && rm -r package
+[ -e package ] && rm -r package
 mkdir -p package/opt
 mkdir -p package/usr/share/applications
 mkdir -p package/usr/share/icons/hicolor/256x256/apps
 
-cp -r dist/ipr package/opt/
-cp src/resources/icons/app/BitCapLngLogo_IPR_Full_BLK-02x256.png package/usr/share/icons/hicolor/256x256/apps/
+cp -r dist/BitCapIPR package/opt/
+cp src/resources/icons/app/BitCapLngLogo_IPR_Full_ORG_BLK-02_Square.png package/usr/share/icons/hicolor/256x256/apps/
 cp ipr.desktop package/usr/share/applications
 
-find package/opt/ipr -type f -exec chmod 644 -- {} +
-find package/opt/ipr -type d -exec chmod 755 -- {} +
+find package/opt/BitCapIPR -type f -exec chmod 644 -- {} +
+find package/opt/BitCapIPR -type d -exec chmod 755 -- {} +
 find package/usr/share -type f -exec chmod 644 -- {} +
-chmod +x package/opt/ipr/ipr
+chmod +x package/opt/BitCapIPR/BitCapIPR
 
-fpm -C package -s dir -t deb -n "$PNAME" -v "$1" -p "$PNAME-$1.deb"
+fpm -C package -s dir -t deb -n "$PNAME" -v "$1" -p "dist/$PNAME-$1.deb"
