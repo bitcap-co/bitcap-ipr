98d77ee0e9becca77bec5f172ccb3283b77ee14b
MatthewWertman <matthewwertman57@gmail.com>
2024-10-17 02:09:39 -0700

    simulate duplicate packets

--- send_udp_dgram.py
+++ send_udp_dgram.py
@@ -1,11 +1,13 @@
 import argparse
 import socket
-
+import sys
+import time
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port', action='store', type=int, required=True)
     parser.add_argument('-m', '--msg', action='store', type=str, required=True)
+    parser.add_argument('-r', '--repeat', action='store', type=int)
     return parser.parse_args()
 
 
@@ -13,4 +15,9 @@ if __name__ == '__main__':
     args = parse_arguments()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    if (args.repeat):
+        for _ in range(args.repeat):
+            sock.sendto(bytes(args.msg, 'utf-8'), ("127.0.0.1", args.port))
+            time.sleep(2)
+        sys.exit(0)
     sock.sendto(bytes(args.msg, 'utf-8'), ("127.0.0.1", args.port))
