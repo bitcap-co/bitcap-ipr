47824852785cb0070a924a6ae30ba5f13d5b102e
MatthewWertman <matthewwertman57@gmail.com>
2025-01-30 00:36:27 -0800

    add initial errors

---
+++ src/mod/api/errors.py
@@ -0,0 +1,7 @@
+class FailedConnectionError(Exception):
+    """Raise for failed connection to miner"""
+
+
+class AuthenticationError(Exception):
+    """Raise for authentication failure to miner"""
+
