d4a52d1c95a73f5a1e9a351fcf74832d172f073c
MatthewWertman <matthewwertman57@gmail.com>
2025-07-25 01:05:25 -0700

    update parser

--- src/mod/api/miners/whatsminer/v3/parser.py
+++ src/mod/api/miners/whatsminer/v3/parser.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from ....parser import Parser
 
 
@@ -6,26 +7,29 @@ class WhatsminerV3Parser(Parser):
         super().__init__(target)
         self.target["algorithm"] = "SHA256"
 
-    def parse_serial(self, obj: dict) -> None:
+    def parse_serial(self, obj: Dict[str, Any]) -> None:
         if "miner-sn" in obj and obj["miner-sn"]:
             self.target["serial"] = obj["miner-sn"]
 
-    def parse_subtype(self, obj: dict) -> None:
+    def parse_subtype(self, obj: Dict[str, Any]) -> None:
         if "type" in obj:
             self.target["subtype"] = obj["type"]
 
-    def parse_firmware(self, obj: dict) -> None:
+    def parse_algorithm(self, obj: Dict[str, Any]) -> None:
+        return super().parse_algorithm(obj)
+
+    def parse_firmware(self, obj: Dict[str, Any]) -> None:
         if "fwversion" in obj:
             self.target["firmware"] = obj["fwversion"]
 
-    def parse_platform(self, obj: dict) -> None:
+    def parse_platform(self, obj: Dict[str, Any]) -> None:
         if "platform" in obj:
             self.target["platform"] = obj["platform"]
 
-    def parse_system_info(self, obj: dict) -> None:
-        self.parse_firmware()
-        self.parse_platform()
+    def parse_system_info(self, obj: Dict[str, Any]) -> None:
+        self.parse_firmware(obj)
+        self.parse_platform(obj)
 
-    def parse_miner_info(self, obj: dict) -> None:
-        self.parse_serial()
-        self.parse_subtype()
+    def parse_miner_info(self, obj: Dict[str, Any]) -> None:
+        self.parse_serial(obj)
+        self.parse_subtype(obj)
