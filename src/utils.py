a13af621f40e218af466ed9a749a40962dbade8c
MatthewWertman <matthewwertman57@gmail.com>
2025-10-11 21:24:05 -0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,16 @@ import sys
 import json
 import collections.abc
 from pathlib import Path
+from typing import Any, Dict
 from platformdirs import user_data_dir, user_log_dir
 from PySide6.QtCore import __version__ as QT_VERSION
 
-APP_INFO = {
+
+CURR_PLATFORM = sys.platform
+BASEDIR = os.path.dirname(__file__)
+IPR_THEME = Path(BASEDIR, "ui", "theme.qss")
+IPR_DEFAULT_CONFIG = Path(BASEDIR, "resources", "app", "config.json.default")
+IPR_METADATA = {
     "name": "BitCap IPReporter",
     "appname": "BitCapIPR",
     "appversion": "1.2.9",
@@ -18,15 +24,13 @@ APP_INFO = {
     "company": "Bit Capital Group",
     "desc": "cross-platform IP reporter that listens for AntMiner, IceRiver, and Whatsminer ASICs.",
 }
-BASEDIR = os.path.dirname(__file__)
-CURR_PLATFORM = sys.platform
 MAX_ROTATE_LOG_FILES = 4
 
 
-def deep_update(d: dict, u: dict) -> dict:
-    for k, v in u.items():
+def deep_update(d: Dict[str, Any], a: Dict[str, Any]) -> Dict[str, Any]:
+    for k, v in a.items():
         if isinstance(v, collections.abc.Mapping):
-            d[k] = deep_update(d.get(k, {}), v)
+            d[k] = deep_update(d.get(k, {}), dict(v))
         # elif isinstance(v, list):
         #     d[k] = (d.get(k, []) + v)
         else:
@@ -34,19 +38,11 @@ def deep_update(d: dict, u: dict) -> dict:
     return d
 
 
-def get_stylesheet():
-    return Path(BASEDIR, "ui", "theme.qss")
-
-
-def get_default_config():
-    return Path(BASEDIR, "resources", "app", "config.json.default")
-
-
 def get_config_dir() -> str:
     if os.path.exists(Path(BASEDIR, "..", "README.md")):
         cd = Path(BASEDIR, "..").as_posix()
     else:
-        cd = user_data_dir(APP_INFO["appname"], APP_INFO["appauthor"])
+        cd = user_data_dir(IPR_METADATA["appname"], IPR_METADATA["appauthor"])
     return cd
 
 
@@ -54,23 +50,11 @@ def get_config_file_path() -> Path:
     return Path(get_config_dir(), "config.json")
 
 
-def read_config(cp: Path) -> dict:
-    with open(cp, "r") as f:
-        c = json.load(f)
-    return c
-
-
-def write_config(cp: Path, content: dict):
-    config = json.dumps(content, indent=2)
-    with open(cp, "w") as f:
-        f.write(config)
-
-
 def get_log_dir() -> str:
     if os.path.exists(Path(BASEDIR, "..", "README.md")):
         ld = Path(BASEDIR, "..", "Logs").as_posix()
     else:
-        ld = user_log_dir(APP_INFO["appname"], APP_INFO["appauthor"])
+        ld = user_log_dir(IPR_METADATA["appname"], IPR_METADATA["appauthor"])
     return ld
 
 
@@ -78,15 +62,19 @@ def get_log_file_path() -> Path:
     return Path(get_log_dir(), "ipr.log")
 
 
+def read_config_file(fp: Path) -> Dict[str, Any]:
+    with open(fp, "r") as f:
+        c = json.load(f)
+    return c
+
+
+def write_config_file(fp: Path, conf: Dict[str, Any]) -> None:
+    c = json.dumps(conf, indent=2)
+    with open(fp, "w") as f:
+        f.write(c)
+
+
 def flush_log():
     with open(get_log_file_path(), "r+") as f:
         f.truncate(0)
         f.seek(0)
-
-
-# def get_miner_url(ip_addr: str, miner_type: str) -> str:
-#     port = 80
-#     match miner_type:
-#         case "dragonball":
-#             port = 16666
-#     return f"http://{ip_addr}:{port}/"
