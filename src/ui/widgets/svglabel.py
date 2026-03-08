ffef205910458674209aa356d05e8569f294b1fd
MatthewWertman <matthewwertman57@gmail.com>
2026-03-07 22:21:35 -0800

    ui: update svglabel widget

--- src/ui/widgets/svglabel.py
+++ src/ui/widgets/svglabel.py
@@ -1,4 +1,3 @@
-from PySide6.QtCore import QResource
 from PySide6.QtGui import QPainter
 from PySide6.QtSvg import QSvgRenderer
 from PySide6.QtWidgets import QLabel
@@ -7,7 +6,7 @@ from PySide6.QtWidgets import QLabel
 class SvgLabel(QLabel):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.__renderer = ""
+        self.__renderer: QSvgRenderer
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -15,7 +14,7 @@ class SvgLabel(QLabel):
             self.__renderer.render(painter)
         return super().paintEvent(event)
 
-    def setSvgFile(self, filename: QResource):
+    def setSvgFile(self, filename: str):
         self.__renderer = QSvgRenderer(filename)
         self.resize(self.__renderer.defaultSize())
         length = max(self.sizeHint().width(), self.sizeHint().height())
