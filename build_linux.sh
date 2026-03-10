3893ed4b56a3cec0b472e9998b1af455083b5dca
MatthewWertman <matthewwertman57@gmail.com>
2026-03-09 23:23:47 -0700

    update linux build

--- build_linux.sh
+++ build_linux.sh
@@ -29,22 +29,22 @@ shift $(( OPTIND - 1 ))
 [[ -z "$VERSION" ]] && echo "ERROR: Supply version string." && exit 1
 
 [ -e dist ] && rm -rf dist
-python3 -m nuitka src/main.py --assume-yes-for-downloads --standalone --output-file=BitCapIPR --output-dir=dist
+python3 -m nuitka src/main.py --assume-yes-for-downloads --standalone --output-file=BitCapIPR --output-dir=dist/BitCapIPR
 
-mv dist/main.dist dist/ipr
-cp README.md CONFIGURATION.md dist
-ln -s ipr/BitCapIPR dist/BitCapIPR
+mv dist/BitCapIPR/main.dist dist/BitCapIPR/bitcap-ipr
+cp README.md CONFIGURATION.md dist/BitCapIPR/
+ln -s bitcap-ipr/BitCapIPR dist/BitCapIPR/BitCapIPR
 (cd dist && zip -r --symlinks "$PNAME-$VERSION-${OS,,}-$PLATFORM-portable.zip" .)
-rm dist/README.md dist/CONFIGURATION.md dist/BitCapIPR
+rm dist/BitCapIPR/README.md dist/BitCapIPR/CONFIGURATION.md dist/BitCapIPR/BitCapIPR
 
 if [[ ARCHIVE -eq 1 ]]; then
-  rm -rf dist/ipr
+  rm -rf dist/BitCapIPR
   exit 0
 fi
 
 # create bitcap-ipr package
 mkdir -p dist/package/DEBIAN
-mkdir -p dist/package/opt/BitCapIPR
+mkdir -p dist/package/opt/bitcap-ipr
 mkdir -p dist/package/usr/share/applications
 mkdir -p dist/package/usr/share/icons/hicolor/128x128/apps
 
@@ -58,16 +58,16 @@ Homepage: https://github.com/bitcap-co/bitcap-ipr
 Description: cross-platform IP reporter that listens for Antminers, IceRivers, and Whatsminers.
 EOF
 
-cp -r dist/ipr dist/package/opt/BitCapIPR
+(cd dist/BitCapIPR/bitcap-ipr && cp -r . ../../package/opt/bitcap-ipr)
 cp resources/app/icons/BitCapLngLogo_IPR_Full_ORG_BLK-02_Square.png dist/package/usr/share/icons/hicolor/128x128/apps/
-cp resources/linux/ipr.desktop dist/package/usr/share/applications/
+cp resources/linux/bitcap-ipr.desktop dist/package/usr/share/applications/
 
-find dist/package/opt/BitCapIPR -type f -exec chmod 644 -- {} +
-find dist/package/opt/BitCapIPR -type d -exec chmod 755 -- {} +
+find dist/package/opt/bitcap-ipr -type f -exec chmod 644 -- {} +
+find dist/package/opt/bitcap-ipr -type d -exec chmod 755 -- {} +
 find dist/package/usr/share -type f -exec chmod 644 -- {} +
-chmod +x dist/package/opt/BitCapIPR/ipr/BitCapIPR
+chmod +x dist/package/opt/bitcap-ipr/BitCapIPR
 
 (cd dist && dpkg-deb --build --root-owner-group package "$PNAME-$VERSION-${OS,,}-$PLATFORM.deb")
 rm -rf dist/package
-rm -rf dist/ipr
+rm -rf dist/BitCapIPR
 exit 0
