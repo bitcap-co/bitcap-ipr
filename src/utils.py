bcf40b2759764b493bea2e11e2c44988fa4a947c
MatthewWertman <matthewwertman57@gmail.com>
2026-04-13 22:28:32 -0700

    bump version

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,7 @@ IPR_DEFAULT_CONFIG = Path(BASEDIR, "resources", "app", "config.json.default")
 IPR_METADATA = {
     "name": "BitCap IPReporter",
     "appname": "BitCapIPR",
-    "appversion": "1.2.9",
+    "appversion": "1.2.10",
     "qt": qVersion(),
     "python": ".".join(map(str, sys.version_info[:3])),
     "appauthor": "BitCap",
