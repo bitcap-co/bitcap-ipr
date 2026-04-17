00cc7849e461f260095dd0156955a64bae953f46
MatthewWertman <matthewwertman57@gmail.com>
2026-04-16 21:29:19 -0700

    ASICClient: set client back to None after closing

--- src/mod/apiv2/client.py
+++ src/mod/apiv2/client.py
@@ -69,7 +69,9 @@ class ASICClient(QObject):
         self, client: BaseHTTPClient | BaseRPCClient | BaseTCPClient
     ):
         # close existing client, if any
-        self.close_client()
+        if self.client is not None:
+            self.client._close()
+            self.client = None
         self.client = client
 
     def client_error(self) -> Exception | None:
