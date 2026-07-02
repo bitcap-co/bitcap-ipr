bdf19d635a3a582f242192be939041290c2743e7
MatthewWertman <matthewwertman57@gmail.com>
2026-07-01 23:32:59 -0700

    ipr: dont scroll to botton on import

--- src/ipr.py
+++ src/ipr.py
@@ -1575,7 +1575,6 @@ class IPR(QMainWindow, Ui_MainWindow):
                     )
                     miner = self._miner_from_data(row)
                     self.id_model.append(miner)
-            self.tableIPRID.scrollToBottom()
         else:
             logger.error(f"import_table : failed to read file {file_name}.")
             self.iprStatusBar.showMessage("Status :: Failed to import table.", 5000)
