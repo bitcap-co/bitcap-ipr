941be2c18504a5cc34049eed639697d50bc2c50e
MatthewWertman <matthewwertman57@gmail.com>
2026-09-06 00:20:48 -0700

    rpc: refactor luxminer

--- src/mod/ipr_asic/rpc/luxminer.py
+++ src/mod/ipr_asic/rpc/luxminer.py
@@ -4,81 +4,16 @@
 # Licensed under the GNU General Public License v3.0; see LICENSE
 
 import logging
-from typing import Literal
+from typing import Literal, override
 
-from pydantic import BaseModel, Field, ValidationError
+from pydantic import TypeAdapter
 
-from mod.ipr_asic.errors import APIError, APIInvalidResponse
-from mod.ipr_asic.models import ActionResponse, BlinkStatus, Status
+from mod.ipr_asic.errors import APIError
 from mod.ipr_asic.rpc.cgminer import CGMinerRPCClient
+from mod.ipr_asic.schemas.luxminer import Config, Dev, Devdetail, MinerPool, Summary
+from mod.ipr_asic.schemas.models import ActionResult, APIObject, BlinkStatus
 
 logger = logging.getLogger(__name__)
-
-
-class ConfigItem(BaseModel):
-    asc_count: int = Field(alias="ASC Count")
-    actual_power_target: int = Field(alias="ActualPowerTarget")
-    bcast_addr: str = Field(alias="BcastAddr")
-    control_board_type: str = Field(alias="ControlBoardType")
-    cooling: str = Field(alias="Cooling")
-    curtail_mode: str = Field(alias="CurtailMode")
-    dhcp: bool = Field(alias="DHCP")
-    dns_servers: str = Field(alias="DNS Servers")
-    device_code: str = Field(alias="Device Code")
-    fpga_build_id_hex: str = Field(alias="FPGABuildIdHex")
-    fpga_build_id_str: str = Field(alias="FPGABuildIdStr")
-    fee_status: str = Field(alias="FeeStatus")
-    gateway: str = Field(alias="Gateway")
-    green_led: str = Field(alias="GreenLed")
-    hostname: str = Field(alias="Hostname")
-    hotplug: str = Field(alias="Hotplug")
-    ip_addr: str = Field(alias="IPAddr")
-    ideal_power_target: int = Field(alias="IdealPowerTarget")
-    immersion_mode: bool = Field(alias="ImmersionMode")
-    is_atm_enabled: bool = Field(alias="IsAtmEnabled")
-    is_power_supply_on: bool = Field(alias="IsPowerSupplyOn")
-    is_power_target_enabled: bool = Field(alias="IsPowerTargetEnabled")
-    is_power_target_supported: bool = Field(alias="IsPowerTargetSupported")
-    is_single_voltage: bool = Field(alias="IsSingleVoltage")
-    is_tuning: bool = Field(alias="IsTuning")
-    log_interval: int = Field(alias="Log Interval")
-    log_file_level: str = Field(alias="LogFileLevel")
-    mac_addr: str = Field(alias="MACAddr")
-    model: str = Field(alias="Model")
-    nameplate_ths: float = Field(alias="NameplateTHS")
-    netmask: str = Field(alias="Netmask")
-    os: str = Field(alias="OS")
-    pga_count: int = Field(alias="PGA Count")
-    pic: str = Field(alias="PIC")
-    psu_hw_version: str = Field(alias="PSUHwVersion")
-    psu_label: str = Field(alias="PSULabel")
-    pool_count: int = Field(alias="Pool Count")
-    power_limit: int = Field(alias="PowerLimit")
-    profile: str = Field(alias="Profile")
-    profile_step: str = Field(alias="ProfileStep")
-    red_led: str = Field(alias="RedLed")
-    serial_number: str = Field(alias="SerialNumber")
-    strategy: str = Field(alias="Strategy")
-    system_status: str = Field(alias="SystemStatus")
-    update_on_startup: str = Field(alias="UpdateOnStartup")
-    update_on_timeout: str = Field(alias="UpdateOnTimeout")
-    update_on_user: str = Field(alias="UpdateOnUser")
-    update_source: str = Field(alias="UpdateSource")
-    update_timeout: int = Field(alias="UpdateTimeout")
-
-
-class ConfigResposnse(BaseModel):
-    config: list[ConfigItem] = Field(alias="CONFIG")
-    status: list[Status] = Field(alias="STATUS")
-    id: int
-
-    def error(self) -> str | None:
-        for status in self.status:
-            match status.status:
-                case "E" | "F":
-                    return f"received API error ({status.code}) {status.msg} - {status.description}"
-                case _:
-                    return None
 
 
 class LuxminerRPCClient(CGMinerRPCClient):
@@ -86,14 +21,16 @@ class LuxminerRPCClient(CGMinerRPCClient):
         super().__init__(ip, port, alt_pwd)
         self.session_token: str | None = None
 
-    async def send_command(self, command: str, *args, **kwargs) -> dict:
+    @override
+    async def send_command(self, command: str, *args, **kwargs) -> APIObject:
         if kwargs.get("parameters") is not None and len(args) == 0:
             return await super().send_command(command, **kwargs)
         return await super().send_command(command, parameters=",".join(args), **kwargs)
 
-    async def send_privileged_command(self, command: str, *args, **kwargs) -> dict:
+    @override
+    async def send_privileged_command(self, command: str, *args, **kwargs) -> APIObject:
         if self.session_token is None:
-            await self.authenticate()
+            _ = await self.authenticate()
         return await self.send_command(command, self.session_token, *args, **kwargs)
 
     async def authenticate(self) -> str | None:
@@ -113,81 +50,76 @@ class LuxminerRPCClient(CGMinerRPCClient):
             pass
         return None
 
-    async def version(self) -> dict:
-        return await super().version()
+    @override
+    async def summary(self) -> Summary:
+        return await self._get_one("summary", "SUMMARY", TypeAdapter(Summary))
 
-    async def summary(self) -> dict:
-        return await super().summary()
+    @override
+    async def devs(self) -> list[Dev]:
+        return await self._get_many("devs", "DEVS", TypeAdapter(list[Dev]))
 
-    async def stats(self) -> list[dict]:
-        return await super().stats()
+    @override
+    async def devdetails(self) -> list[Devdetail]:
+        return await self._get_many(
+            "devdetails", "DEVDETAILS", TypeAdapter(list[Devdetail])
+        )
 
-    async def devs(self) -> list[dict]:
-        return await super().devs()
+    @override
+    async def pools(self) -> list[MinerPool]:
+        return await self._get_many("pools", "POOLS", TypeAdapter(list[MinerPool]))
 
-    async def devdetails(self) -> list[dict]:
-        return await super().devdetails()
+    @override
+    async def get_system_info(self) -> Config:
+        return await self._get_one("config", "CONFIG", TypeAdapter(Config))
 
-    async def pools(self) -> list[dict]:
-        return await super().pools()
-
-    async def get_system_info(self) -> dict:
-        resp = await self.send_command("config")
-        try:
-            resobj = ConfigResposnse.model_validate(obj=resp, by_alias=True)
-        except ValidationError as e:
-            logger.error(f"{self.__repr__()} : {APIInvalidResponse(reason=str(e))!s}")
-            raise APIInvalidResponse
-        else:
-            err = resobj.error()
-            if err:
-                logger.error(f"{self.__repr__()} : {APIError(err)!s}")
-                raise APIError("Command failed!")
-            return resp["CONFIG"][0]
-
-    async def get_pool_conf(self) -> list[dict]:
-        return await super().get_pool_conf()
-
-    async def get_blink_status(self) -> dict:
+    async def get_blink_status(self) -> BlinkStatus:
         resp = await self.get_system_info()
-        blink = BlinkStatus(blink=resp["RedLed"] == "blink")
-        return blink.model_dump()
+        blink = BlinkStatus(blink=resp.red_led == "blink")
+        return blink
 
+    @override
     async def blink(
         self,
         enabled: bool,
         auto: bool = True,
         led: str = "red",
-    ) -> dict:
+    ) -> APIObject:
         if enabled:
             auto = False
         if auto:
             return await self.send_privileged_command("ledset", led, "auto")
         return await self.send_privileged_command("ledset", led, "blink")
 
-    async def set_miner_mode(self, mode: Literal["sleep", "wakeup"] = "wakeup") -> dict:
+    async def set_miner_mode(
+        self, mode: Literal["sleep", "wakeup"] = "wakeup"
+    ) -> APIObject:
         resp = await self.send_privileged_command("curtail", mode)
-        resobj = self._validate_response(resp)
-        return resobj.model_dump()
+        error = self._validate_response(resp)
+        if error is not None:
+            logger.error(f"{self.__repr__()}: {APIError(error)!s}")
+            raise APIError("Command failed")
+        return resp
 
-    async def start(self) -> dict:
+    @override
+    async def start(self) -> APIObject:
         return await self.set_miner_mode("wakeup")
 
-    async def stop(self) -> dict:
+    @override
+    async def stop(self) -> APIObject:
         return await self.set_miner_mode("sleep")
 
-    async def restart(self) -> dict:
+    @override
+    async def restart(self) -> APIObject:
         return await self.send_privileged_command("resetminer")
 
-    async def reboot(self) -> dict:
+    @override
+    async def reboot(self) -> APIObject:
         return await self.send_privileged_command("rebootdevice")
 
-    async def update_passwd(self, old_passwd: str, new_passwd: str) -> dict:
-        return await super().update_passwd(old_passwd, new_passwd)
-
+    @override
     async def update_pool_conf(
         self, urls: list[str], users: list[str], passwds: list[str]
-    ) -> dict:
+    ) -> APIObject:
         if len(urls) != 3 or len(users) != 3 or len(passwds) != 3:
             raise APIError("Invalid length of arguments")
 
@@ -196,6 +128,9 @@ class LuxminerRPCClient(CGMinerRPCClient):
                 continue
             pool: list[str] = [urls[i], users[i], passwds[i]]
             resp = await self.send_command("addpool", *pool)
-            _ = self._validate_response(resp)
-        resobj = ActionResponse(success=True, msg="OK")
+            error = self._validate_response(resp)
+            if error is not None:
+                logger.error(f"{self.__repr__()}: {APIError(error)!s}")
+                raise APIError("Command failed")
+        resobj = ActionResult(success=True, msg="OK")
         return resobj.model_dump()
