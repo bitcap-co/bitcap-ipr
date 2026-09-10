34bfa9e6ef3b1715c3d4c4bfc4df1c1ae11aeca9
MatthewWertman <matthewwertman57@gmail.com>
2026-09-10 01:02:20 -0700

    ipr_asic: refactor whatsminer

--- src/mod/ipr_asic/data/miners/whatsminer.py
+++ src/mod/ipr_asic/data/miners/whatsminer.py
@@ -3,135 +3,125 @@
 # This file is part of bitcap-ipr
 # Licensed under the GNU General Public License v3.0; see LICENSE
 
-from typing import Any
 
-from mod.ipr_asic.data import BaseParser, MinerAlgorithm, MinerFirmware, MinerType
+from pydantic import BaseModel
+
+from mod.ipr_asic.data import (
+    MinerAlgorithm,
+    MinerData,
+    MinerFirmware,
+    MinerType,
+)
+from mod.ipr_asic.schemas.whatsminer import (
+    BTMinerDevDetails as WhatsminerDevDetails,
+)
+from mod.ipr_asic.schemas.whatsminer import (
+    BTMinerPool as WhatsminerPool,
+)
+from mod.ipr_asic.schemas.whatsminer import (
+    BTMinerSummary as WhatsminerSummary,
+)
+from mod.ipr_asic.schemas.whatsminer import (
+    BTMinerSystemInfo as WhatsminerSystemInfo,
+)
+from mod.ipr_asic.schemas.whatsminer import (
+    BTMinerV3DeviceInfo as WhatsminerV3DeviceInfo,
+)
+from mod.ipr_asic.schemas.whatsminer import (
+    BTMinerV3Pool as WhatsminerV3Pool,
+)
+from mod.ipr_asic.schemas.whatsminer import (
+    BTMinerV3Summary as WhatsminerV3Summary,
+)
+from mod.ipr_asic.schemas.whatsminer import (
+    BTMinerVersion as WhatsminerVersionInfo,
+)
 
 
-class WhatsminerParser(BaseParser):
-    def __init__(self) -> None:
-        super().__init__()
-        self.data.type = MinerType.WHATSMINER
-        self.data.firmware = MinerFirmware.STOCK
-        self.data.algorithm = MinerAlgorithm.SHA256
+class WhatsminerModels(BaseModel):
+    system_info: WhatsminerSystemInfo
+    summary: WhatsminerSummary
+    version_info: WhatsminerVersionInfo
+    pools: list[WhatsminerPool]
+    dev_details: list[WhatsminerDevDetails]
 
-    def parse_api_version(self, obj: dict[str, Any]) -> None:
-        self.data.api_version = obj["api_ver"]
 
-    def parse_uptime(self, obj: Any) -> None:
+class WhatsminerV3Models(BaseModel):
+    device_info: WhatsminerV3DeviceInfo
+    summary: WhatsminerV3Summary
+    pools: list[WhatsminerV3Pool]
+
+
+class WhatsminerParser:
+    def parse(self, models: WhatsminerModels) -> MinerData:
+        data = MinerData()
+        data.type = MinerType.WHATSMINER
+        data.firmware = MinerFirmware.STOCK
+        data.algorithm = MinerAlgorithm.SHA256
+
+        data.api_version = models.version_info.api_ver
         try:
-            self.data.uptime = int(obj["Elapsed"])
+            data.uptime = int(models.summary.elapsed)
         except ValueError:
-            return
+            data.uptime = None
+        data.hostname = models.system_info.hostname
+        data.mac = models.system_info.mac
+        data.serial = models.system_info.minersn
+        data.fw_version = models.version_info.fw_ver
+        data.platform = models.version_info.platform
 
-    def parse_hostname(self, obj: dict[str, Any]) -> None:
-        self.data.hostname = obj["hostname"]
+        miner_type = models.version_info.miner_type
+        if miner_type is not None:
+            data.subtype = miner_type
+        else:
+            # get from devices
+            data.subtype = models.dev_details[0].model
 
-    def parse_mac(self, obj: Any) -> None:
-        self.data.mac = obj["mac"]
-
-    def parse_serial(self, obj: dict[str, Any]) -> None:
-        self.data.serial = obj["minersn"]
-
-    def parse_type(self, obj: Any) -> None:
-        return super().parse_type(obj)
-
-    def parse_subtype(self, obj: list[dict]) -> None:
-        dev = obj[0]
-        self.data.subtype = dev["Model"]
-
-    def parse_algorithm(self, obj: Any) -> None:
-        return super().parse_algorithm(obj)
-
-    def parse_firmware(self, obj: Any) -> None:
-        self.data.fw_version = obj["fw_ver"]
-
-    def parse_platform(self, obj: Any) -> None:
-        self.data.platform = obj["platform"]
-
-    def parse_system_info(self, obj: Any) -> None:
-        self.parse_hostname(obj)
-        self.parse_mac(obj)
-        self.parse_serial(obj)
-
-    def parse_summary(self, obj: Any) -> None:
-        self.parse_uptime(obj)
-
-    def parse_pools(self, obj: list[dict[str, Any]]) -> None:
-        for pool in obj:
-            if pool["Status"] == "Alive":
-                self.data.stratum_url = pool["URL"]
-                if "." in pool["User"]:
-                    user, worker = pool["User"].split(".", 1)
-                    self.data.username = user
-                    self.data.worker_name = worker
+        for pool in models.pools:
+            if pool.status == "Alive":
+                data.stratum_url = pool.url
+                if "." in pool.user:
+                    user, worker = pool.user.split(".", 1)
+                    data.username = user
+                    data.worker_name = worker
                 else:
-                    self.data.username = pool["User"]
+                    data.username = pool.user
                 break
 
-    def parse_version_info(self, obj: dict[str, Any]) -> None:
-        self.parse_api_version(obj)
-        self.parse_firmware(obj)
-        self.parse_platform(obj)
+        return data
 
 
-class WhatsminerV3Parser(BaseParser):
-    def __init__(self) -> None:
-        super().__init__()
-        self.data.type = MinerType.WHATSMINER
-        self.data.firmware = MinerFirmware.STOCK
-        self.data.algorithm = MinerAlgorithm.SHA256
+class WhatsminerV3Parser:
+    def parse(self, models: WhatsminerV3Models) -> MinerData:
+        data = MinerData()
+        data.type = MinerType.WHATSMINER
+        data.firmware = MinerFirmware.STOCK
+        data.algorithm = MinerAlgorithm.SHA256
 
-    def parse_api_version(self, obj: Any) -> None:
-        self.data.api_version = obj["system"]["api"]
+        system = models.device_info.system
+        if system is not None:
+            data.api_version = system.api
+            data.fw_version = system.fwversion
+            data.platform = system.platform
+        data.uptime = models.summary.elapsed
+        network = models.device_info.network
+        if network is not None:
+            data.hostname = network.hostname
+            data.mac = network.mac
+        miner = models.device_info.miner
+        if miner is not None:
+            data.serial = miner.miner_sn
+            data.subtype = miner.type
 
-    def parse_uptime(self, obj: Any) -> None:
-        self.data.uptime = obj["summary"]["elapsed"]
-
-    def parse_hostname(self, obj: Any) -> None:
-        self.data.hostname = obj["network"]["hostname"]
-
-    def parse_mac(self, obj: Any) -> None:
-        self.data.mac = obj["network"]["mac"]
-
-    def parse_serial(self, obj: Any) -> None:
-        self.data.serial = obj["miner"]["miner-sn"]
-
-    def parse_type(self, obj: Any) -> None:
-        return super().parse_type(obj)
-
-    def parse_subtype(self, obj: Any) -> None:
-        self.data.subtype = obj["miner"]["type"]
-
-    def parse_algorithm(self, obj: Any) -> None:
-        return super().parse_algorithm(obj)
-
-    def parse_firmware(self, obj: Any) -> None:
-        self.data.fw_version = obj["system"]["fwversion"]
-
-    def parse_platform(self, obj: Any) -> None:
-        self.data.platform = obj["system"]["platform"]
-
-    def parse_system_info(self, obj: Any) -> None:
-        self.parse_api_version(obj)
-        self.parse_hostname(obj)
-        self.parse_mac(obj)
-        self.parse_platform(obj)
-        self.parse_firmware(obj)
-        self.parse_serial(obj)
-        self.parse_subtype(obj)
-
-    def parse_summary(self, obj: Any) -> None:
-        self.parse_uptime(obj)
-
-    def parse_pools(self, obj: list[dict[str, Any]]) -> None:
-        for pool in obj:
-            if pool["status"] == "alive":
-                self.data.stratum_url = pool["url"]
-                if "." in pool["account"]:
-                    user, worker = pool["account"].split(".", 1)
-                    self.data.username = user
-                    self.data.worker_name = worker
+        for pool in models.pools:
+            if pool.status == "alive":
+                data.stratum_url = pool.url
+                if "." in pool.account:
+                    user, worker = pool.account.split(".", 1)
+                    data.username = user
+                    data.worker_name = worker
                 else:
-                    self.data.username = pool["account"]
+                    data.username = pool.account
                 break
+
+        return data
