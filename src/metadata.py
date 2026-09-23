e3f1663f074156244960db0cd19cd92ecd69ca80
MatthewWertman <matthewwertman57@gmail.com>
2026-09-22 22:15:08 -0700

    bump base version v1.5.3

--- src/metadata.py
+++ src/metadata.py
@@ -8,7 +8,7 @@
 APP_METADATA: dict[str, str] = {
     "name": "BitCap IPReporter",
     "appname": "BitCapIPR",
-    "appversion": "1.5.2",
+    "appversion": "1.5.3",
     "appauthor": "BitCap",
     "author": "MatthewWertman",
     "source": "https://github.com/bitcap-co/bitcap-ipr",
